8b2129e5510bd1ed1995f14b088260794682fe43
Vegard Seines <vegsei@gmail.com>
2023-07-28 12:09:44 +0200

    added home-manager as input

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs }: 
+  outputs = inputs@{ self, nixpkgs, home-manager }: 
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
