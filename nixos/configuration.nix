ebd7479e476317dd559544b59be9426c842f736c
Vegard Seines <vegsei@gmail.com>
2023-07-30 21:42:26 +0200

    cleaned up more, allow unfree pkgs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,52 +1,40 @@
+#<---------------------------------------------->#
+#               NixOS Config         -> by Vegard#
+#<---------------------------------------------->#
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    [
+# Include the results of the hardware scan.
+    ./hardware-configuration.nix
     ];
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  # For connecting airpods
-  # hardware.bluetooth.settings = { General = { ControllerMode = "bredr"; }; };
-  # Set your time zone.
-  time.timeZone = "Europe/Oslo";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Enable the X11 windowing system.
+#<---------------------------------------------->|
+#             Gnome Settings                     |
+#<---------------------------------------------->|
+# Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
+# Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
+# Configure keymap in X11 
   services.xserver = {
     layout = "no";
     xkbVariant = "";
   };
+  services.gnome.core-utilities.enable = false;
 
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
-  ];
-  # Enable CUPS to print documents.
+
+
+#<---------------------------------------------->|
+#             other Settings                     |
+#<---------------------------------------------->|
+# Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+# Enable CUPS to print documents.
   services.printing.enable = false;
-
-  # Enable sound with pipewire.
+# Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -56,11 +44,46 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  networking.hostName = "nixos"; # Define your hostname.
+# Enable networking
+    networking.networkmanager.enable = true;
+# Set your time zone.
+  time.timeZone = "Europe/Oslo";
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
+# Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+# For connecting airpods
+# hardware.bluetooth.settings = { General = { ControllerMode = "bredr"; }; };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+# Enable the OpenSSH daemon.
+# services.openssh.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+#<---------------------------------------------->|
+#             Packages                           |
+#<---------------------------------------------->|
+#  Fonts:
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+  ];
+  environment.systemPackages = with pkgs; [
+    vim
+    home-manager
+    git
+    zsh
+  ];
+# List services that you want to enable:
+  programs.zsh.enable = true;
+
+
+
+
+
+# Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.vegard = {
     isNormalUser = true;
     description = "Vegard Seines";
@@ -70,35 +93,14 @@
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  services.gnome.core-utilities.enable = false;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  vim
-  git
-  home-manager
-  zsh
-  ];
-  # List services that you want to enable:
-  programs.zsh.enable = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
 }
