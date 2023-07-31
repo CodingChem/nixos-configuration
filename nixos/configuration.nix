08b328068752b93174c13c3a9b8661b85024e073
Vegard Seines <vegsei@gmail.com>
2023-07-31 08:05:02 +0200

    modularized desktop-environment setup: Gnome

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,29 +8,13 @@
     [
 # Include the results of the hardware scan.
     ./hardware-configuration.nix
+#================================================#
+# Include desktop-environment
+#------------------------------------------------#
+#              Gnome                             #
+    ../desktop-environment/gnome.nix
+#------------------------------------------------#
     ];
-
-#<---------------------------------------------->|
-#             Gnome Settings                     |
-#<---------------------------------------------->|
-# Enable the X11 windowing system.
-  services.xserver.enable = true;
-# Remove Xterm:
-  services.xserver.excludePackages = [ pkgs.xterm ];
-  services.xserver.desktopManager.xterm.enable = false;
-# Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-# Configure keymap in X11 
-  services.xserver = {
-    layout = "no";
-    xkbVariant = "";
-  };
-  services.gnome.core-utilities.enable = false;
-  environment.gnome.excludePackages = with pkgs; [
-    gnome-tour
-  ];
-
 
 #<---------------------------------------------->|
 #             other Settings                     |
