d5f242aa1d76e346edcd2aeffe6049e59ccf62d3
Vegard Seines <vegsei@gmail.com>
2023-07-31 10:11:19 +0200

    Moved directories and cleaned up configuration file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,24 +6,32 @@
 {
   imports =
     [
+#================================================
 # Include the results of the hardware scan.
     ./hardware-configuration.nix
-#================================================#
-# Include desktop-environment
-#------------------------------------------------#
-#              Gnome                             #
-    ../desktop-environment/gnome.nix
-#------------------------------------------------#
+#================================================
+# Desktop-environment
+#------------------------------------------------
+#   Gnome                              
+#------------------------------------------------    
+    ./desktop-environment/gnome.nix
+#------------------------------------------------
     ];
-
-#<---------------------------------------------->|
-#             other Settings                     |
-#<---------------------------------------------->|
-# Select internationalisation properties.
+#================================================|
+# System Settings                                |
+#------------------------------------------------|
+#   Bootloader settings                |
+#-------------------------------------------------
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+#-------------------------------------------------
+#   Language & Region          
+#-------------------------------------------------
   i18n.defaultLocale = "en_US.UTF-8";
-# Enable CUPS to print documents.
-  services.printing.enable = false;
-# Enable sound with pipewire.
+  time.timeZone = "Europe/Oslo";
+#-------------------------------------------------
+#   Sound
+#-------------------------------------------------
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -33,27 +41,22 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-# Enable networking
-    networking.networkmanager.enable = true;
-# Set your time zone.
-  time.timeZone = "Europe/Oslo";
-# Enable touchpad support (enabled default in most desktopManager).
-# services.xserver.libinput.enable = true;
-# Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-# For connecting airpods
+# For connecting airpods or other tricky bluetooth devices, uncomment the following line
+# After pairing the devices this can be commented again
 # hardware.bluetooth.settings = { General = { ControllerMode = "bredr"; }; };
+#-------------------------------------------------
+#   Networking                 
+#-------------------------------------------------
+  networking.hostName = "nixos"; 
+  networking.networkmanager.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 # Enable the OpenSSH daemon.
 # services.openssh.enable = true;
-
-#<---------------------------------------------->|
-#             Packages                           |
-#<---------------------------------------------->|
+#------------------------------------------------
+#   System Packages                           
+#------------------------------------------------
+# Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 #  Fonts:
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
@@ -64,14 +67,17 @@
     git
     zsh
   ];
-# List services that you want to enable:
+#------------------------------------------------
+#   Services                           
+#------------------------------------------------
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
+# Enable CUPS to print documents.
+  services.printing.enable = false;
   programs.zsh.enable = true;
-
-
-
-
-
-# Define a user account. Don't forget to set a password with ‘passwd’.
+#------------------------------------------------
+#   Users                              
+#------------------------------------------------
   users.users.vegard = {
     isNormalUser = true;
     description = "Vegard Seines";
@@ -80,15 +86,6 @@
     packages = with pkgs; [
     ];
   };
-
-
-
-# This value determines the NixOS release from which the default
-# settings for stateful data, like file locations and database versions
-# on your system were taken. It‘s perfectly fine and recommended to leave
-# this value at the release version of the first install of this system.
-# Before changing this value read the documentation for this option
-# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
+#------------------------------------------------
+  system.stateVersion = "23.05"; # Dont touch
 }
